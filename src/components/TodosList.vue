0d3b9960ccc9c99befbd386bcf9b475bb58d2733
Micha Alt <mail@lean-stack.rocks>
2020-01-06 22:04:21 +0100

    Static component tree created.

---
+++ src/components/TodosList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="todo-list">
+        <TodosItem />
+    </ul>
+</template>
+
+<script>
+    import TodosItem from "./TodosItem";
+    export default {
+        name: "TodosList",
+        components: {TodosItem}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodosShell.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="todoapp">
+        <header class="header">
+            <h1>todos</h1>
+            <TodosInput />
+        </header>
+        <TodosMain />
+        <TodosActionbar />
+    </section>
+</template>
+
+<script>
+    import TodosInput from "./TodosInput";
+    import TodosMain from "./TodosMain";
+    import TodosActionbar from "./TodosActionbar";
+    export default {
+        name: "TodosShell",
+        components: {TodosActionbar, TodosMain, TodosInput}
+    }
+</script>
+
+<style scoped>
+
+</style>
