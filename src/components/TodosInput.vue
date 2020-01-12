26a2912e2535541897647386b26e97914c019455
Micha Alt <mail@lean-stack.rocks>
2020-01-12 15:33:47 +0100

    Child-to-Parent communication via events.

--- src/components/TodosInput.vue
+++ src/components/TodosInput.vue
@@ -1,10 +1,22 @@
 <template>
-    <input class="new-todo" placeholder="What needs to be done?" autofocus>
+    <input v-model="title" v-on:keyup.enter="createTodo" class="new-todo" placeholder="What needs to be done?" autofocus>
 </template>
 
 <script>
     export default {
-        name: "TodosInput"
+        name: "TodosInput",
+        data: () => ({
+            title: ''
+        }),
+        methods: {
+            createTodo: function () {
+                const title = this.title.trim();
+                if (title.length > 0) {
+                    this.$emit('create', title);
+                    this.title = '';
+                }
+            }
+        }
     }
 </script>
 
--- src/components/TodosShell.vue
+++ src/components/TodosShell.vue
@@ -2,7 +2,7 @@
     <section class="todoapp">
         <header class="header">
             <h1>todos</h1>
-            <TodosInput />
+            <TodosInput v-on:create="store.createTodo($event)" />
         </header>
         <TodosMain />
         <TodosActionbar />
@@ -13,9 +13,13 @@
     import TodosInput from "./TodosInput";
     import TodosMain from "./TodosMain";
     import TodosActionbar from "./TodosActionbar";
+    import {store} from "../state/store";
     export default {
         name: "TodosShell",
-        components: {TodosActionbar, TodosMain, TodosInput}
+        components: {TodosActionbar, TodosMain, TodosInput},
+        data: () => ({
+            store: store
+        })
     }
 </script>
 
