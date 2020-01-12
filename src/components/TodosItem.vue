ef825c3ef771479529cf3b43950cb1818d7b5960
Micha Alt <mail@lean-stack.rocks>
2020-01-12 14:33:41 +0100

    Basic template syntax.

--- src/components/TodosItem.vue
+++ src/components/TodosItem.vue
@@ -1,18 +1,46 @@
 <template>
-    <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-    <li class="completed">
+    <li v-bind:class="{ completed: todo.completed, editing: editMode }">
         <div class="view">
-            <input class="toggle" type="checkbox" checked>
-            <label>Taste JavaScript</label>
-            <button class="destroy"></button>
+            <input class="toggle" type="checkbox" v-on:change="toggleCompletedState" v-bind:checked="todo.completed">
+            <label v-on:dblclick="beginEdit">{{ todo.title }}</label>
+            <button class="destroy" v-on:click="destroyTodo"></button>
         </div>
-        <input class="edit" value="Create a TodoMVC template">
+        <input ref="editFld" class="edit" v-model="editText"
+            v-on:keyup.enter="commitEdit" v-on:keyup.esc="cancelEdit" v-on:blur="commitEdit"
+        >
     </li>
 </template>
 
 <script>
     export default {
-        name: "TodosItem"
+        name: "TodosItem",
+        data: () => ({
+            editMode: false,
+            editText: '',
+            todo: { id: 17, title: 'Template Syntax', completed: false }
+        }),
+        methods: {
+            toggleCompletedState: function () {
+                this.todo.completed = !this.todo.completed;
+            },
+            destroyTodo: function () {
+
+            },
+            beginEdit: function () {
+                this.editMode = true;
+                this.editText = this.todo.title;
+                this.$nextTick(() => this.$refs.editFld.focus());
+            },
+            commitEdit: function () {
+                if (this.editMode) {
+                    this.editMode = false;
+                    this.todo.title = this.editText.trim();
+                }
+            },
+            cancelEdit: function () {
+                this.editMode = false;
+            }
+        }
     }
 </script>
 
