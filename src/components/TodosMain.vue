8b1ed5d4de26285018ad5eaaa88cc453f9055efc
Micha Alt <mail@lean-stack.rocks>
2020-01-12 21:24:14 +0100

    Main component done.

--- src/components/TodosMain.vue
+++ src/components/TodosMain.vue
@@ -1,9 +1,9 @@
 <template>
-    <!-- This section should be hidden by default and shown when there are todos -->
-    <section class="main">
-        <input id="toggle-all" class="toggle-all" type="checkbox">
+    <section v-if="hasTodos" class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox"
+               v-bind:checked="allTodosComplete" v-on:change="syncAllCompletedStates">
         <label for="toggle-all">Mark all as complete</label>
-        <TodosList v-bind:todos="state.todos" />
+        <TodosList v-bind:todos="store.state.todos" />
     </section>
 </template>
 
@@ -14,8 +14,24 @@
         name: "TodosMain",
         components: {TodosList},
         data: () => ({
-          state: store.state
-        })
+            todos: store.state.todos,
+            store: store
+        }),
+        computed: {
+            hasTodos: function () {
+                return this.todos.length > 0;
+            },
+            allTodosComplete: function () {
+                return this.todos.findIndex(t => !t.completed) === -1;
+            }
+        },
+        methods: {
+            syncAllCompletedStates: function () {
+                this.todos.filter(t => t.completed === this.allTodosComplete).forEach(
+                    t => this.store.toggleTodo(t)
+                )
+            }
+        }
     }
 </script>
 
